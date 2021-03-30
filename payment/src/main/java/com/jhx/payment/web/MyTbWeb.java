e4a69d0068351bbf54d8c65b8f3a93a07e6806e4
iosyyy <626797813@qq.com>
2021-03-30 21:05:44 +0800

    init

--- payment/src/main/java/com/jhx/payment/web/MyTbWeb.java
+++ payment/src/main/java/com/jhx/payment/web/MyTbWeb.java
@@ -5,12 +5,14 @@ import com.jhx.payment.po.ResultMap;
 import com.jhx.payment.service.MyTbService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.Random;
+import java.util.List;
 
 /**
  * @author 靖鸿宣
@@ -25,30 +27,48 @@ public class MyTbWeb {
     @Value("${server.port}")
     private String serverPort;
 
+    @Resource
+    private DiscoveryClient discoveryClient;
+
     @PostMapping("/payment/insertMyTb")
-    public ResultMap InsertMyTb(MyTb myTb){
-        log.info("insert MyTb start..."+myTb);
+    public ResultMap InsertMyTb(MyTb myTb) {
+        log.info("insert MyTb start..." + myTb);
         int insert = myTbService.insert(myTb);
-        if (insert==0){
-            log.error("insert"+myTb+" failed");
-            return new ResultMap(0,"insert"+myTb+" failed",insert);
-        }else{
-            log.info("insert"+myTb+"success");
-            return new ResultMap(500,"insert"+myTb+"success,serverPort:"+serverPort,insert);
+        if (insert == 0) {
+            log.error("insert" + myTb + " failed");
+            return new ResultMap(0, "insert" + myTb + " failed", insert);
+        } else {
+            log.info("insert" + myTb + "success");
+            return new ResultMap(500, "insert" + myTb + "success,serverPort:" + serverPort, insert);
         }
     }
 
     @GetMapping("/payment/selectMyTb")
-    public ResultMap SelectMyTb(Integer mId){
+    public ResultMap SelectMyTb(Integer mId) {
 
-        log.info("select MyTb start..."+mId);
+        log.info("select MyTb start..." + mId);
         MyTb myTb = myTbService.selectByPrimaryKey(mId);
-        if (myTb==null){
-            log.error("select"+mId+" failed");
-            return new ResultMap(0,"select"+mId+" failed,serverPort:"+serverPort,myTb);
-        }else{
-            log.info("select"+mId+"success");
-            return new ResultMap(500,"select"+mId+"success,serverPort:"+serverPort,myTb);
+        if (myTb == null) {
+            log.error("select" + mId + " failed");
+            return new ResultMap(0, "select" + mId + " failed,serverPort:" + serverPort, myTb);
+        } else {
+            log.info("select" + mId + "success");
+            return new ResultMap(500, "select" + mId + "success,serverPort:" + serverPort, myTb);
         }
+    }
+
+    @GetMapping("/payment/discovery")
+    public Object discovery() {
+        for (String service : discoveryClient.getServices()) {
+            log.info("...service{}", service);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("cloud-payment-service");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId() + " " +
+                    instance.getHost() + ":" + instance.getPort()
+                    + " " + instance.getUri()
+            );
+        }
+        return discoveryClient;
     }
 }
--- payment3/src/main/java/com/demo/test/Application.java
+++ payment3/src/main/java/com/demo/test/Application.java
@@ -1,21 +1,17 @@
-package com.jhx.payment;
+package com.demo.test;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author 靖鸿宣
- * @since 2021/3/24
+ * @since 2021/3/30
  */
 @SpringBootApplication
-@MapperScan({"com.jhx.payment.dao"})
-@EnableEurekaClient
 @EnableDiscoveryClient
-public class PaymentApplication {
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentApplication.class,args);
+        SpringApplication.run(Application.class,args);
     }
 }
